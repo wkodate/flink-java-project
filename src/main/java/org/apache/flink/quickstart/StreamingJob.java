237588f105e4e33ced3a054067dd56a5056efa50
wkodate <hbwandeow@gmail.com>
2018-03-05 23:53:13 +0900

    create DataStream used by taxi ride source

--- src/main/java/org/apache/flink/quickstart/StreamingJob.java
+++ src/main/java/org/apache/flink/quickstart/StreamingJob.java
@@ -8,9 +8,9 @@ package org.apache.flink.quickstart;
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,56 +18,56 @@ package org.apache.flink.quickstart;
  * limitations under the License.
  */
 
+import com.dataartisans.flinktraining.exercises.datastream_java.datatypes.TaxiRide;
+import com.dataartisans.flinktraining.exercises.datastream_java.sources.TaxiRideSource;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 
 /**
  * Skeleton for a Flink Streaming Job.
- *
+ * <p>
  * <p>For a full example of a Flink Streaming Job, see the SocketTextStreamWordCount.java
  * file in the same package/directory or have a look at the website.
- *
+ * <p>
  * <p>You can also generate a .jar file that you can submit on your Flink
  * cluster.
  * Just type
- * 		mvn clean package
+ * mvn clean package
  * in the projects root directory.
  * You will find the jar in
- * 		target/flink-java-project-0.1.jar
+ * target/flink-java-project-0.1.jar
  * From the CLI you can then run
- * 		./bin/flink run -c org.apache.flink.quickstart.StreamingJob target/flink-java-project-0.1.jar
- *
+ * ./bin/flink run -c org.apache.flink.quickstart.StreamingJob target/flink-java-project-0.1.jar
+ * <p>
  * <p>For more information on the CLI see:
- *
+ * <p>
  * <p>http://flink.apache.org/docs/latest/apis/cli.html
  */
 public class StreamingJob {
 
-	public static void main(String[] args) throws Exception {
-		// set up the streaming execution environment
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+    public static void main(String[] args) throws Exception {
+        String sourcePath = args[0];
 
-		/**
-		 * Here, you can start creating your execution plan for Flink.
-		 *
-		 * Start with getting some data from the environment, like
-		 * 	env.readTextFile(textPath);
-		 *
-		 * then, transform the resulting DataStream<String> using operations
-		 * like
-		 * 	.filter()
-		 * 	.flatMap()
-		 * 	.join()
-		 * 	.coGroup()
-		 *
-		 * and many more.
-		 * Have a look at the programming guide for the Java API:
-		 *
-		 * http://flink.apache.org/docs/latest/apis/streaming/index.html
-		 *
-		 */
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        int maxDelay = 1;
+        int servingSpeed = 1;
+        DataStream<TaxiRide> rides = env.addSource(new TaxiRideSource(sourcePath, maxDelay, servingSpeed));
 
-		// execute program
-		env.execute("Flink Streaming Java API Skeleton");
-	}
+        /**
+         rides.filter()
+         .flatMap()
+         .join()
+         */
+
+        rides.print();
+        env.execute("Flink Streaming Java API Skeleton");
+
+        // Example output
+        // 1> 936656,START,2013-01-01 00:00:00,1970-01-01 00:00:00,-73.9867,40.751236,-74.00318,40.72036,2
+        // 1> 928769,START,2013-01-01 00:00:00,1970-01-01 00:00:00,-73.99078,40.76088,-73.954185,40.778847,1
+        // 1> 899206,START,2013-01-01 00:00:00,1970-01-01 00:00:00,-73.97391,40.747868,-73.9489,40.77665,5
+    }
 }
--- src/main/java/org/apache/flink/quickstart/WordCount.java
+++ src/main/java/org/apache/flink/quickstart/WordCount.java
@@ -1,22 +1,5 @@
 package org.apache.flink.quickstart;
 
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.DataSet;
@@ -24,23 +7,7 @@ import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.util.Collector;
 
-/**
- * Implements the "WordCount" program that computes a simple word occurrence histogram
- * over some sample data
- *
- * <p>This example shows how to:
- * <ul>
- * <li>write a simple Flink program.
- * <li>use Tuple data types.
- * <li>write and use user-defined functions.
- * </ul>
- *
- */
 public class WordCount {
-
-    //
-    //	Program
-    //
 
     public static void main(String[] args) throws Exception {
 
@@ -56,26 +23,14 @@ public class WordCount {
         );
 
         DataSet<Tuple2<String, Integer>> counts =
-                // split up the lines in pairs (2-tuples) containing: (word,1)
                 text.flatMap(new LineSplitter())
-                        // group by the tuple field "0" and sum up tuple field "1"
                         .groupBy(0)
                         .sum(1);
 
-        // execute and print result
         counts.print();
 
     }
 
-    //
-    // 	User Functions
-    //
-
-    /**
-     * Implements the string tokenizer that splits sentences into words as a user-defined
-     * FlatMapFunction. The function takes a line (String) and splits it into
-     * multiple pairs in the form of "(word,1)" (Tuple2&lt;String, Integer&gt;).
-     */
     public static final class LineSplitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
 
         @Override
